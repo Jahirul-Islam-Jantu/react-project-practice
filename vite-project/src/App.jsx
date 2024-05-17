0ed9b2ae9a481327f296e31103cdfc3305d81da3
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-05-17 11:58:04 +0600

    props

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,11 +3,13 @@ import Header from "./components/Header";
 import Result from "./components/Result";
 import ContactForm from "./components/ContactForm";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
 
 const App = () => {
   let marks = 85;
   return (
     <div>
+      <Home title="This is home title" des="This is description" />
       <Header />
       <Hero />
       {marks >= 80 ? <h1>Brilliant Result</h1> : <h2>Average Result</h2>}
---
+++ vite-project/src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <p>{props.des}</p>
+    </div>
+  );
+};
+
+export default Home;
